e7e25e23c149d073a93f3ddc74541171c4a6d270
Nishith G. Behera <zenolusx@gmail.com>
2020-04-19 19:28:11 +0530

    Add IDE Custom test functionality

--- src/components/problem/ide.js
+++ src/components/problem/ide.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 import '../../css/ide.css'
 import Select from 'react-select'
 import AceEditor from 'react-ace'
-
 import 'ace-builds/src-noconflict/ext-language_tools'
 import 'ace-builds/src-noconflict/mode-c_cpp'
 import 'ace-builds/src-noconflict/mode-python'
@@ -11,12 +10,12 @@ import 'ace-builds/src-noconflict/theme-monokai'
 import 'ace-builds/src-noconflict/theme-github'
 import 'ace-builds/src-noconflict/theme-solarized_dark'
 import 'ace-builds/src-noconflict/theme-terminal'
-import { Button, Spinner } from 'react-bootstrap'
+import { Button, Spinner, Modal, Table } from 'react-bootstrap'
 
 const request = require('request')
 
 class IDE extends Component{
-  modes = [{value: "c_cpp", label: "C/C++", language: "cpp17", versionIndex: 0}, {value: "java", language: "java", label: "Java", versionIndex: 3}, {value: "python", label: "Python3", language: "python3", versionIndex: 3}]
+  modes = [{value: "c_cpp", label: "C/C++", language: "cpp"}, {value: "java", language: "java", label: "Java"}, {value: "python", label: "Python3", language: "python3"}]
   selected = 0
   constructor(props){
     super(props)
@@ -27,6 +26,7 @@ class IDE extends Component{
       stdin : ``,
       stdout : ``,
       executing : false,
+      showRun : false,
     }
   }
 
@@ -41,26 +41,24 @@ class IDE extends Component{
   onRun = () => {
     this.setState({executing: true, stdout: `Compiling and Executing your program...`})
     request({
-      url: 'https://codered-web-server.herokuapp.com/api/execute',
-      method: "POST",
-      json: {
-        script: this.state.script,
-        language: this.modes[this.selected].language,
-        versionIndex: this.modes[this.selected].versionIndex,
-        stdin: this.state.stdin
-      }
+      url: `http://api.paiza.io:80/runners/create?source_code=${encodeURIComponent(this.state.script)}&language=${this.modes[this.selected].language}&input=${encodeURIComponent(this.state.stdin)}&api_key=guest`,
+      method: "POST"
     }, (error, response, body) => {
-      if(response)
-        this.setState({stdout: response.body.output || ``, executing: false})
+      const id = (JSON.parse(response.body))["id"];
+      setTimeout(() => {
+        request(`http://api.paiza.io:80/runners/get_details?id=${id}&api_key=guest`, (err, res, body) => {
+          this.setState({stdout: (JSON.parse(res.body))["stdout"], executing: false})
+        })
+      }, 3000)
     })
   }
   onSubmit = () => {
-
+    
   }
 
   render() {
     return(
-      <div className = "ide bottomPadding" id = "ide" style = {{height: "100%", width: "100%"}}>
+      <div className = "ide" id = "ide" style = {{height: "100%", width: "100%"}}>
         <span>Language: </span>
         <span><Select
           className = "inline langSelect"
@@ -73,7 +71,7 @@ class IDE extends Component{
         /></span>
         <span style = {{textAlign: "right"}}>
           {this.state.executing ? <Spinner animation = "grow" size = "sm" variant = "danger"/>:
-            <Button className = "actions" id = {this.state.executing?"running":"run"} type = "button" disabled = {this.state.executing} onClick = {this.onRun}>Custom Test</Button>}
+            <Button className = "actions" id = {this.state.executing?"running":"run"} type = "button" disabled = {this.state.executing} onClick = {() => this.setState({showRun: true})}>Custom Test</Button>}
           <Button className = "actions" id = "submit" type = "button" onClick = {this.onSubmit}>Submit</Button>
           <Button className = "actions" id = "clear" type = "button" onClick = {this.onClear}>Clear</Button>
         </span>
@@ -92,9 +90,36 @@ class IDE extends Component{
           value = {this.state.script}
           onChange = {this.onScriptChange}
         />
+        <CustomTest show = {this.state.showRun} onHide = {() => this.setState({showRun: false})} stdin = {this.state.stdin} stdout = {this.state.stdout} onStdInChange = {this.onStdInChange} onRun = {this.onRun} executing = {this.state.executing}/>
       </div>
     )
   }
+}
+
+const CustomTest = props => {
+  return(
+    <Modal show = {props.show} onHide = {props.onHide} aria-labelledby = "contained-modal-title-vcenter" centered>
+      <Modal.Body style = {{backgroundColor: "#101820FF", color: "whitesmoke"}}>
+        <Table className="center wd65" borderless>
+          <thead><tr>
+            <th>StdIn</th>
+            <th>StdOut</th>
+          </tr></thead>
+        </Table>
+        <div className = "IO center wd65">
+          <div className = "center inline wd50">
+            <textarea className = "inp noLeftBorder" placeholder = " Enter custom input" value = {props.stdin} onChange = {props.onStdInChange} />
+          </div>
+          <div className = "center inline wd50">
+            <textarea className = "out noRightBorder" placeholder = " Run to generate output" value = {props.stdout} readOnly = {true} />
+          </div>
+        </div>
+        <div style = {{width: "100%", textAlign: "center"}}>
+          <Button className = "actions" style = {{marginLeft: "auto", marginRight: "auto"}} id = {props.executing?"running":"run"} type = "button" disabled = {props.executing} onClick = {props.onRun}>Run Test</Button>
+        </div>
+      </Modal.Body>
+    </Modal>
+  )
 }
 
 export default IDE
